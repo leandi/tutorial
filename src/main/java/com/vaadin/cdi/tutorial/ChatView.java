6737b18c18c15bb38d81d1a6c3929a3950a63db5
Sandi Kurtin <sandi.kurtin@gmai.com>
2017-01-25 17:45:38 +0100

    Verzija kjer III/3A  deluje vse ok - Chatview

--- src/main/java/com/vaadin/cdi/tutorial/ChatView.java
+++ src/main/java/com/vaadin/cdi/tutorial/ChatView.java
@@ -4,15 +4,20 @@ import javax.inject.Inject;
 
 import com.vaadin.cdi.CDIView;
 import com.vaadin.cdi.internal.Conventions;
+import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Layout;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -26,6 +31,12 @@ public class ChatView extends CustomComponent implements View {
     private UserInfo userInfo;
 	
     private User targetUser;
+    
+    private Layout messageLayout;
+    
+    @Inject
+    private javax.enterprise.event.Event<Message> messageEvent;
+
 
     @Override
     public void enter(ViewChangeEvent event) {
@@ -58,14 +69,48 @@ public class ChatView extends CustomComponent implements View {
 
     private Layout buildUserLayout() {
         VerticalLayout layout = new VerticalLayout();
-        layout.setWidth("100%");
+        layout.setSizeFull();
         layout.setMargin(true);
         layout.setSpacing(true);
         layout.addComponent(new Label("Talking to " + targetUser.getName()));
         layout.addComponent(generateBackButton());
+        layout.addComponent(buildChatLayout(targetUser));
         return layout;
     }
+    private Component buildChatLayout(final User targetUser) {
+        VerticalLayout chatLayout = new VerticalLayout();
+        chatLayout.setSizeFull();
+        chatLayout.setSpacing(true);
+        messageLayout = new VerticalLayout();
+        messageLayout.setWidth("100%");
+        final TextField messageField = new TextField();
+        messageField.setWidth("100%");
+        final Button sendButton = new Button("Send");
+        sendButton.addClickListener(new ClickListener() {
 
+            @Override
+            public void buttonClick(ClickEvent event) {
+                String message = messageField.getValue();
+                if (!message.isEmpty()) {
+                        messageField.setValue("");
+                        messageEvent.fire(new Message(userInfo.getUser(),
+                                        targetUser, message));
+                }
+            }
+        });
+        sendButton.setClickShortcut(KeyCode.ENTER);
+        Panel messagePanel = new Panel(messageLayout);
+        messagePanel.setHeight("400px");
+        messagePanel.setWidth("100%");
+        chatLayout.addComponent(messagePanel);
+        HorizontalLayout entryLayout = new HorizontalLayout(sendButton,
+                messageField);
+        entryLayout.setWidth("100%");
+        entryLayout.setExpandRatio(messageField, 1);
+        entryLayout.setSpacing(true);
+        chatLayout.addComponent(entryLayout);
+        return chatLayout;
+    }
     private Layout buildUserSelectionLayout() {
         VerticalLayout layout = new VerticalLayout();
         layout.setWidth("100%");
